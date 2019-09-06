be79a1c3fca71ebffff86394a3ee2e0396350a6e
doominator42 <dominic@germain.co>
2019-09-06 15:04:02 -0400

    Create project and test repro

---
+++ MaxValidationDepthBugRepro/Models/BugReproModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MaxValidationDepthBugRepro.Models
+{
+    public class BugReproModel
+    {
+        [Required]
+        public string Prop { get; set; }
+    }
+}
